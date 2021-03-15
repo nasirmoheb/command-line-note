4f4143b26c7dd6cace2c90a172b335389bae24f8
Nasir ahmad Moheb <nasir.moheb111@gmail.com>
2021-03-15 23:35:51 +0430

    =Adding functionality of reading and removing  of a note

--- note-app/app.js
+++ note-app/app.js
@@ -1,7 +1,7 @@
 //requiring modules
+const { log } = require('console');
 const yargs = require('yargs');
-var notes = require('./notes.js')
-
+var notes = require('./notes.js');
 
 var argv = yargs.argv;
 
@@ -11,67 +11,62 @@ var argv = yargs.argv;
     3. Hint: use yarqs options.
 */
 
-
 var command = argv._[0];
 
-if(command == 'add'){
-    note = notes.addNotes(argv.title, argv.body);
-    if(note){
-        console.log('Note created');
-        notes.logNote(note)
-    }else{
-        console.log('Note already exist.');
-    }
-}
-
-else if(command === 'read'){
-
-    /* --Instruction--
+if (command == 'add') {
+  note = notes.addNotes(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    notes.logNote(note);
+  } else {
+    console.log('Note already exist.');
+  }
+} else if (command === 'read') {
+  /* --Instruction--
         1. Get a title from the command line and pass to the getNote() function inside notes.js.
         2. Print the note if the note is found
         3. Print 'Note note found.'
        
     */
 
-        
-
-    /* --Code--
+  /* --Code--
       write your code here
     */
-}
-else if(command === 'list'){
-    allNotes = notes.getAll();
+  const note = notes.getNote(argv.title);
+  if (note) {
+    notes.logNote(note);
+  } else {
+    console.log('Note note found.');
+  }
+} else if (command === 'list') {
+  allNotes = notes.getAll();
 
-
-    /* --Instruction--
+  /* --Instruction--
         1. Print the number of notes (How many notes exist in the file.)
     */
 
-
-    /* --Code---
+  /* --Code---
         write your code here
     */
 
-    allNotes.forEach((note) => {
-        notes.logNote(note)
-    });
-}
-else if(command === 'remove'){
-
-    /* --Instruction--
+  allNotes.forEach((note) => {
+    notes.logNote(note);
+  });
+} else if (command === 'remove') {
+  /* --Instruction--
         1. Get a title from the command line
         2. Pass the title to removeNote function in notes.js
         3. Give use the feedback wether or not the note was removed.
     */
-
-    
-    /* --Code---
+  /* --Code---
         write your code here
     */
-
-
+  const feedback = notes.removeNote(argv.title);
+  if (feedback) {
+    console.log('Note removed');
+  } else {
+    console.log('Note does not removed');
+  }
+} else {
+  console.log('command is not recognized.');
 }
-else{
-    console.log('command is not recognized.')
-}
-
--- note-app/notes.js
+++ note-app/notes.js
@@ -1,84 +1,99 @@
 const fs = require('fs');
 
 var fetchNote = () => {
-    try {
-
-        var notesString = fs.readFileSync('notes-data.json');
-        return JSON.parse(notesString);
-
-    } catch (error) {
-       return [];
-    }
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (error) {
+    return [];
+  }
 };
 
 var saveNote = (notes) => {
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 var addNotes = (title, body) => {
-    var notes = fetchNote()
-    var note = {
-        title,
-        body
-    };
- 
-    /* --instruction
+  var notes = fetchNote();
+  var note = {
+    title,
+    body,
+  };
+
+  /* --instruction
         check if a note already exists?
         if yes; then return 'true'
         if not; save the note. and print the newly added note.
     */
 
-        /* --code
-
+  /* --code
             write your code here
-
         */
 
-    saveNote(notes)
-    return note;
-    
-}
+  for (let n of notes) {
+    if (n.title === title) {
+      return false;
+    }
+  }
+
+  notes.push(note);
+  saveNote(notes);
+  return note;
+};
 
 var getAll = () => {
-    return fetchNote();
-}
+  return fetchNote();
+};
 
 var getNote = (title) => {
-
-    /* --Instruction--
+  /* --Instruction--
         1. Fetch the notes
         2. Filter notes get the one with title of argument
         3. Return the note if found it.
     */
-   
-    /* --Code--
+
+  /* --Code--
       write your code here
     */
-  
+
+  const notes = fetchNote();
+  for (let note of notes) {
+    if (note.title === title) {
+      return note;
+    }
+  }
 };
 
 var removeNote = (title) => {
-      /* --Instruction--
+  /* --Instruction--
         1. fetch the notes
         2. filter notes, remove the one with title of the argument.
         3. save new notes array.
       */
-    
-
-    /* --Code--
+  /* --Code--
       write your code here
     */
-   
+  const notes = fetchNote();
+
+  for (let note of notes) {
+    if (title === note.title) {
+      notes.splice(notes.indexOf(notes), 1);
+      saveNote(notes);
+      return true;
+    }
+  }
 };
 
-var logNote = (note) =>{
-    console.log('--');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
-}
+var logNote = (note) => {
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+};
 
 module.exports = {
-    addNotes,
-    getAll,
-    logNote
-}
+  addNotes,
+  getAll,
+  logNote,
+  getNote,
+  removeNote,
+};
